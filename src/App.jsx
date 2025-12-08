3e2ede28021b59c29579abb461f9ade7472475c1
Alexander <test@tests-MacBook-Air.local>
2025-12-08 12:24:57 +0200

    Footer & responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SliderSection from './components/SliderSection'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from './components/Footer'
 
 
 
@@ -19,7 +20,7 @@ const App = () => {
             <AiSection />
             <SliderSection />
             <TestimonialSection />
-
+            <Footer />
         </>
     )
 }
